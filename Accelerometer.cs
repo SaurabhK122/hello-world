f34b7a56d2cb83fa68aca2c68f9afadf83bf1abc
Saurabh Khandelwal <khandelwal.saurabh31@gmail.com>
2019-07-13 11:06:13 +0530

    This commit includes the code with allows you to view the object in camera and simultaneously view the sensor data.  The sensor data I have included is that of Accelerometer, Compass, GPS and Gyroscope

---
+++ Accelerometer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Accelerometer : MonoBehaviour
+{
+    public static Accelerometer Instance { set; get; }
+
+    public float accX;
+    public float accY;
+    public float accZ;
+
+    private void Start()
+    {
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+    }
+
+    private void Update()
+    {
+        accX = Input.acceleration.x;
+        accY = Input.acceleration.y;
+        accZ = Input.acceleration.z;
+    }
+}
---
+++ Compass.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Compass : MonoBehaviour
+{
+    public static Compass Instance { set; get; }
+
+    public float compassX;
+    public float compassY;
+    public float compassZ;
+
+    private void Start()
+    {
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+        Input.compass.enabled = true;
+    }
+
+    private void Update()
+    {
+
+        compassX = Input.compass.rawVector.x;
+        compassY = Input.compass.rawVector.y;
+        compassZ = Input.compass.rawVector.z;
+    }
+}
